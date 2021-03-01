f90508d4c0ac6e9f09deff64a28ad184dae0d463
Can Ahmet ACAR <64232218+canacar7@users.noreply.github.com>
2021-03-02 00:40:57 +0300

    Add files via upload

---
+++ adc_ex/adc_pot.c
@@ -0,0 +1,49 @@
+#include "stm32f10x.h"                  // Device header
+#include "stm32f10x_adc.h"              // Keil::Device:StdPeriph Drivers:ADC
+
+void gpioConfig(){
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;         //analog veri okucamiz icin analog in sectik
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_0;
+	
+	GPIO_Init(GPIOA,&GPIO_InitStructure);                 //speed kullanmadik veri okumasi icin
+
+}
+void adcConfig(){
+	ADC_InitTypeDef ADC_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
+	
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;  									//sürekli cerim olsun istedimizden enable
+	ADC_InitStructure.ADC_DataAlign          = ADC_DataAlign_Right; 			//en degerlikli bitin ne tarafa dayali olmasi gereklidir dio
+	ADC_InitStructure.ADC_ExternalTrigConv   = ADC_ExternalTrigConv_None;	//trigger kulanmmasak none olur
+	ADC_InitStructure.ADC_Mode               = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_NbrOfChannel       = 1;                          //kaç kanal oldugunu söyler 1 adc kullandimiz icin 1 tane
+	ADC_InitStructure.ADC_ScanConvMode       =  DISABLE;									 //çoklu modda kullaniliyor gelen verilerin taranmasini sagliyor
+	
+	ADC_Init(ADC1,&ADC_InitStructure);
+	ADC_Cmd(ADC1,ENABLE);
+}
+
+uint16_t readADC(){
+ADC_RegularChannelConfig(ADC1,ADC_Channel_0,1,ADC_SampleTime_55Cycles5);
+//adc nin türü, adc nin channel i, renk (birden fazla adc de  okuma önceligi icin), 1 veriiyi kaç cycle da okumasi gerekli
+ADC_SoftwareStartConvCmd(ADC1,ENABLE);  //ADC MIZI BASLATIRKEN HANGI ADC OLDUGUNU BELIRTIK VE IIZN VERIDK
+
+while(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC) == RESET);
+return ADC_GetConversionValue(ADC1);
+
+}
+
+uint16_t adc_value;
+int main(){
+
+gpioConfig();
+adcConfig();
+while(1){
+
+	adc_value = readADC();
+}
+
+}
